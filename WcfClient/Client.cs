c5af1b93cfd69b0f3fbd40af7607e8571bde6e35
André Steenhoff Madsen <andre@steenhoff.dk>
2018-01-04 14:28:06 +0100

    that is it for today

--- WcfClient/Client.cs
+++ WcfClient/Client.cs
@@ -8,6 +8,8 @@ namespace WcfClient
 {
     class Client
     {
+        // chage the name afther the '.' when a better name is given to the service classes
+        WcfService.Service1Client service = new WcfService.Service1Client();
         static void Main(string[] args)
         {
         }
